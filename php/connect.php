0b63e7bcbc97613bec992d7731036b36bcf71b47
Lucas Fleury <lucas.fleury@cpe.fr>
2022-02-04 12:08:06 +0100

    connection bdd

---
+++ php/connect.php
@@ -0,0 +1,21 @@
+<?php
+
+$serveur_bd = 'localhost';
+$nom_bd = 'acudb';
+$utilisateur_bd = 'pgtidal';
+$passe_bd= 'tidal';
+$source_bd = "pgsql:host=$serveur_bd;port=5432;dbname=$nom_bd;user=$utilisateur_bd;password=$passe_bd";
+
+try{
+    $dbh = new PDO($source_bd);
+    if($dbh){
+        echo "<script> console.log(\"Connection à la BDD $nom_bd réussi\") </script>";
+    }
+}catch (PDOException $e){ // Génération d'une exception PHP PDO PostgreSQL
+    echo $e->getCode() . ' ' . $e->getMessage();
+}
+
+$sql = 'SELECT * FROM maTable WHERE monChamp = :maVar';
+$sth = $dbh->prepare($sql);
+$sth->execute(array(':maVar' => "vraie valeur"));
+$data = $sth->fetchAll();
