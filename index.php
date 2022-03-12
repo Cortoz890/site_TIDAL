26f218ccc4c636fe682c2417a7db2b31bea8aec8
Cortoz890 <corto.deschamps@club-internet.fr>
2022-03-12 18:11:07 +0100

    connection quasi finie

--- index.php
+++ index.php
@@ -8,5 +8,7 @@ $smarty->setCompileDir('/var/www/html/template_c');
 $smarty->setConfigDir('/var/www/html/configs');
 $smarty->setCacheDir('/var/www/html/cache');
 
+$smarty->assign('name', "jean");
+
 $smarty->display('index.tpl');
 $smarty->display('footer.tpl');
